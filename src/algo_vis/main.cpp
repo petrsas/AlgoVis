c84f4d67ff32accfff2bde2d36139ed68dd6df0f
petrsas <petr.chyla@post.cz>
2026-08-10 14:40:05 +0200

    proper templating required

--- src/algo_vis/main.cpp
+++ src/algo_vis/main.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <format>
 #include <ranges>
+#include <mutex>
 
 
 #define WIDTH 1280
@@ -22,6 +23,64 @@
 //#define OUTLINE_COLOR
 
 constexpr std::chrono::duration<double> HOLD_TIME{0.5};
+
+
+class ThreadSafeLogger {
+//most likely
+    static inline std::vector<std::string> logs;
+    static inline std::mutex mutex;
+public:
+    static void log_alloc(const char* name, const size_t count, const size_t size) {
+        std::lock_guard<std::mutex> lock(mutex); //auto unlock when out of scope
+        logs.emplace_back(std::format("Allocated {} of {} requiring {} bytes\n", count, name, size));
+    }
+    static void print_log() {
+        for (const std::string& l : logs) {
+            std::cout<<l;
+        }
+    }
+};
+
+//noexcept gurantees that no exceptions will be thrown by that method
+//gurantee can be broken, it which case the program will just end immidiately without handling the exception
+// = default means that compiler will generate the implementation of the constructor, ie. sets members with their default initializers or leaves unit primitives
+//this simple stateless allocator does not throw exceptions
+//for constr because then std containers can just swap pointers instead of creating full copies during moving
+//for dealloc because std containers destructors are noexcept as cleanup ops are simply not allowed to fail ...
+//std::terminate is called on dual exceptions or on exception thrown in noexcept function, which terminates the program at once to prevent propagation of assumed corrupted internal state (like writing corrupted data on disk)
+template <typename T>
+class LoggingAllocator {
+public:
+    using value_type = T;
+
+    LoggingAllocator() noexcept = default;
+
+    template <typename U>
+    LoggingAllocator(const LoggingAllocator<U>&) noexcept {}
+
+    T* allocate(size_t n) {
+        ThreadSafeLogger::log_alloc(typeid(T).name(), n, n * sizeof(T));
+        //creates instace of std::allocator on stack
+        //allocates n elems of type T on heap via the allocate(n) method
+        //returns a pointer to that mem
+        return std::allocator<T>{}.allocate(n); 
+    }
+
+    void deallocate(T* p, std::size_t n) noexcept {
+        //creates an instance of std::allocator on stack
+        //dealloc n elems of type T from heap starting at p
+        std::allocator<T>{}.deallocate(p, n);
+    }
+
+    //hidden friend, typename T is already known
+    //also the compiler does not have to consider this on every == operation
+    //lack of named params, since they are not used inside the function
+    //params are there only to satisfy the compiler which reqs a specific sig
+    template <typename U>
+    friend bool operator==(const LoggingAllocator&, const LoggingAllocator<U>&) { return true; }
+    template < typename U>
+    friend bool operator!=(const LoggingAllocator&, const LoggingAllocator<U>&) { return false; }
+};
 
 class Utils {
 public:
@@ -89,14 +148,15 @@ public:
     }
 };
 
+template <typename Alloc>
 class VisualVector {
 public:
-    std::vector<int> vec;
+    std::vector<int, Alloc> vec;
     sf::RenderTarget& target;
     std::vector<std::string> instrs;
 
 public:
-    VisualVector(sf::RenderTarget& target, std::vector<int>& vec)
+    VisualVector(sf::RenderTarget& target, const std::vector<int, Alloc>& vec)
         : vec(vec), target(target) {
         std::string vec_str;
 
@@ -340,7 +400,7 @@ void BubbleSortOptimized(VisualVector& vec) {
 }
 
 int main() {
-    std::vector<int> vec = {42, 15, 88, 7, 23, 91, 34, 62, 5, 50, 19, 76, 12, 99, 31, 54, 8, 67, 45, 20};
+    std::vector<int, LoggingAllocator<int>> vec = {42, 15, 88, 7, 23, 91, 34, 62, 5, 50, 19, 76, 12, 99, 31, 54, 8, 67, 45, 20};
 
     // SFML 3.1 uses initializer lists for VideoMode dimensions
     auto window = sf::RenderWindow(sf::VideoMode({WIDTH, HEIGHT}), "Algorithm Visualizer by Petr Chyla");
@@ -378,3 +438,5 @@ int main() {
     }
     return 0;
 }
+
+//New TP: template properly...
